130679749c728f96f2dd0a59b8285104b0690b2d
Dr.C <dclinkenbeard@csumb.edu>
2019-09-09 18:49:24 -0700

    Comparing two strings... incorrectly.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String name1 = "Carol Danvers";
+        String name2 = name1;
+
+        System.out.println("Name1 == " + name1);
+        System.out.println("Name2 == " + name2);
+        System.out.println("Name1 == Name 2" + name1==name2);
+    }
+}
