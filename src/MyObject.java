789ea260dfc1049125dad49f58133710b62f4476
Dr.C <dclinkenbeard@csumb.edu>
2019-09-10 12:06:06 -0700

    Completed code, I used the auto generated code for all of these.

--- src/MyObject.java
+++ src/MyObject.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class MyObject {
 
     String name;
@@ -8,12 +10,42 @@ public class MyObject {
         this.count = 0;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
     @Override
     public String toString() {
         return "MyObject{" +
                 "name='" + name + '\'' +
                 ", count=" + count +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyObject myObject = (MyObject) o;
+        return Objects.equals(name, myObject.name) &&
+                Objects.equals(count, myObject.count);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, count);
     }
 
     public static void main(String[] args) {
